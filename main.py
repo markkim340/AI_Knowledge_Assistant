61fdba720af7cf30282a05be9018f73cbbad8d07
mark_kim <mark340v@gmail.com>
2023-11-26 19:35:01 +0900

    이전 커밋 수정소스 반영

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from dotenv import load_dotenv
-load_dotenv()
+# from dotenv import load_dotenv
+# load_dotenv()
 
-# __import__('pysqlite3')
-# import sys
-# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
